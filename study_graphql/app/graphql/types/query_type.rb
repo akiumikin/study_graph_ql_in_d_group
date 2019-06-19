c46e906667c615b0c53c123b3b239f54682f211a
金彰海 <>
2019-06-19 23:50:37 +0900

    refs #9 単体のレコードをgraph-qlのqueryで取得してみる

--- study_graphql/app/graphql/types/query_type.rb
+++ study_graphql/app/graphql/types/query_type.rb
@@ -1,13 +1,11 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :user, Types::UserType, null: false, description: 'ユーザー' do
+      argument :id, ID, required: true
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def user(id:)
+      User.find_by(id: id)
     end
   end
 end
