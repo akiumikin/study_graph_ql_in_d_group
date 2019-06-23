8d77af18a60e2e94fc5d40ceef598a0f76bfdebc
金彰海 <>
2019-06-23 20:30:02 +0900

    refs #17 userのcolumnおよびrelationの取得に対するrspecを追加

---
+++ study_graphql/spec/requests/graphql/types/tag_type_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe '【tag_type query via user】', type: :request do
+  let!(:user) { FactoryBot.create(:user) }
+
+  context 'tagsの取得の場合' do
+    before do
+      query = <<-QUERY
+        {
+          user(id: "#{user.id}") {
+            id
+            tags {
+              id
+              name
+              createdAt
+              updatedAt
+            }
+          }
+        }
+      QUERY
+      @post = post(graphql_path, params: { query: query })
+      @json = JSON.parse(response.body, symbolize_names: true)
+    end
+
+    it 'tagsの[id, name, createdAt, updatedAt]が返ること' do
+      res_data = @json[:data][:user][:tags]
+      expect_data = graphql_format(user.tags)
+
+      expect(@post).to eq 200
+      expect(res_data).to eq expect_data
+    end
+  end
+end
+
+# 他のfield越しに取得可能な場合には増やす
+# RSpec.describe 'tag_type query via xxxx', type: :request do
+#   let!(:xxxx) { FactoryBot.create(:xxxx) }
+# end
---
+++ study_graphql/spec/requests/graphql/types/user_type_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe '【user_type query】', type: :request do
+  let!(:user) { FactoryBot.create(:user) }
+
+  # リレーションのテストは各リレーション先のtype_spec
+  context 'Userの取得の場合' do
+    before do
+      query = <<-QUERY
+        {
+          user(id: "#{user.id}") {
+            id
+            name
+            createdAt
+            updatedAt
+          }
+        }
+      QUERY
+      @post = post(graphql_path, params: { query: query })
+      @json = JSON.parse(response.body, symbolize_names: true)
+    end
+
+    it '[id, name, createdAt, updatedAt]が返ること' do
+      res_data = @json[:data][:user]
+      expect_data = graphql_format(user)
+
+      expect(@post).to eq 200
+      expect(res_data).to eq expect_data
+    end
+  end
+end
