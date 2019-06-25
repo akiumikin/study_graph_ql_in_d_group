0ae774e155c2f77e4b713f68a86275993304f7ca
金彰海 <>
2019-06-26 08:12:14 +0900

    refs #18 upsert_userの更新時の戻り値を修正

--- study_graphql/app/models/user.rb
+++ study_graphql/app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :blogs, dependent: :destroy
   has_many :user_tag_relations
   has_many :tags, through: :user_tag_relations
+
+  # update!をした後の戻り値にactive_recordの形を取らせる
+  def update_with_return_value(update_hash = {})
+    return if update_hash.blank?
+
+    self.update!(update_hash) # これでreturnするとtrue/falseが返る
+    self
+  end
 end
